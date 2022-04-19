2fe4edb619cc278c37c6d60f12fb5fa8429c55c4
Simon <simon@rspsystems.com>
2022-04-19 09:33:42 +0200

    ComponentLoader unit test

--- tests/unit/graphics/test-componentloader.cpp
+++ tests/unit/graphics/test-componentloader.cpp
@@ -17,23 +17,41 @@ using namespace ::rsp::graphics;
 
 TEST_CASE("Component Loader Test")
 {
+    // Arrange
     auto begin = std::chrono::high_resolution_clock::now();
+
+    // Act
     ComponentLoader loader("testFiles/Test keybord - Sprite sheet.bmp",
                            "testFiles/GuiItemsPlacement.csv");
 
-    std::filesystem::path p = rsp::posix::FileSystem::GetCharacterDeviceByDriverName("vfb2", std::filesystem::path{"/dev/fb?"});
-    Framebuffer fb(p.empty() ? nullptr : p.string().c_str());
-
-    // Can we draw the bitmaps?
-    for (auto comp : loader.GetAllComponents()) {
-        fb.DrawImage(comp.second.mPlacement, comp.second.mBitmap);
-    }
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
-    std::cout << "Duration: " << duration << std::endl;
 
-    // Component comp = loader.GetComponent("Keyboard_number");
-    // fb.DrawImage(comp.mPlacement, comp.mBitmap);
-    fb.SwapBuffer(BufferedCanvas::SwapOperations::Clear);
-    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+    // Assert
+    CHECK(duration < 1000);
+    CHECK(loader.GetAllComponents().size() == 206);
+    MESSAGE("Duration: " << duration);
+
+    SUBCASE("Components Contain pixels")
+    {
+        // Arrange
+        int i = 0;
+        int rand = std::rand() % (loader.GetAllComponents().size() + 1);
+
+        // Act & Assert
+        for (auto comp : loader.GetAllComponents()) {
+            if (i == rand) {
+                CHECK(comp.second.mBitmap.GetPixels().size() > 0);
+            }
+        }
+        SUBCASE("Draw sectioned Components")
+        {
+            std::filesystem::path p = rsp::posix::FileSystem::GetCharacterDeviceByDriverName("vfb2", std::filesystem::path{"/dev/fb?"});
+            Framebuffer fb(p.empty() ? nullptr : p.string().c_str());
+            for (auto comp : loader.GetAllComponents()) {
+                fb.DrawImage(comp.second.mPlacement, comp.second.mBitmap);
+            }
+            fb.SwapBuffer(BufferedCanvas::SwapOperations::Clear);
+        }
+    }
 }
